80ea4bb9810ab5640c31b3cc489af993bbe6c689
zxlgit1807 <zxl719@qq.com>
2018-07-17 18:35:24 +0800

    新增user

---
+++ src/main/java/com/zxl/dao/UserMapper.java
@@ -0,0 +1,30 @@
+package com.zxl.dao;
+
+import com.zxl.model.User;
+import com.zxl.model.UserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserMapper {
+    long countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(String id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(String id);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
